92c1b1add8833e85513420803834dd63c7d7a65e
Osvaldo <osvaldobbini@gmail.com>
2025-04-09 22:47:11 -0300

    add models

--- src/models/base.py
+++ src/models/base.py
@@ -9,19 +9,17 @@ class Ticket(Base):
   id: Mapped[str] = mapped_column(String, primary_key=True)
   title: Mapped[str] = mapped_column(String)
   description: Mapped[str] = mapped_column(String)
-  area: Mapped[str] = mapped_column(String)  
+  area_id: Mapped[str] = mapped_column(String) #id from area  
 
 class Technician(Base):
   __tablename__ = "technician"
   id: Mapped[str] = mapped_column(String, primary_key=True)
   name: Mapped[str] = mapped_column(String)
-  area: Mapped[str] = mapped_column(String)
-  email: Mapped[str] = mapped_column(String)
+  area_id: Mapped[str] = mapped_column(String) #id from area
+  email: Mapped[str] = mapped_column(String) 
 
 class Area(Base):
   __tablename__ = "area"
   id: Mapped[str] = mapped_column(String, primary_key=True)
   name: Mapped[str] = mapped_column(String)
   description: Mapped[str] = mapped_column(String)
-
-#add relation between area and technicians
