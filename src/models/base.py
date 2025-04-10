35709a31868380c98d5a6d805ae633838b3da9f6
Osvaldo <osvaldobbini@gmail.com>
2025-04-09 22:42:15 -0300

    add models

---
+++ src/models/base.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+class Ticket(Base):
+  __tablename__ = "tickets"
+  id: Mapped[str] = mapped_column(String, primary_key=True)
+  title: Mapped[str] = mapped_column(String)
+  description: Mapped[str] = mapped_column(String)
+  area: Mapped[str] = mapped_column(String)  
+
+class Technician(Base):
+  __tablename__ = "technician"
+  id: Mapped[str] = mapped_column(String, primary_key=True)
+  name: Mapped[str] = mapped_column(String)
+  area: Mapped[str] = mapped_column(String)
+  email: Mapped[str] = mapped_column(String)
+  
